989c8d8b83a86fdac4052b5b936ce527e1cef417
aheadx <250828230@qq.com>
2020-04-21 09:00:11 +0800

    短信验证

---
+++ tensquare_parent53/tensquare_sms/src/main/java/com/tensquare/sms/SmsApplication.java
@@ -0,0 +1,14 @@
+package com.tensquare.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by aheadx on 2020/4/16.
+ */
+@SpringBootApplication
+public class SmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApplication.class);
+    }
+}
